21cad7db5efac1a4654f88e601b80f69313b5563
alex <alexeleave@gmail.com>
2022-02-20 18:13:53 +0400

    Parallel Tasks

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 export const getLatestNews = async (searchQuery) => {
-  console.log(searchQuery);
-  const request = await fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}&hitsPerPage=10&page=0`);
+  const request = await fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}&hitsPerPage=10&page=0`);
+  return await request.json();
+}
+
+export const getPopularNews = async () => {
+  const request = await fetch(`https://hn.algolia.com/api/v1/search?query=&hitsPerPage=10&page=0`);
   return await request.json();
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,18 +1,28 @@
-import { take, put, call, takeEvery, takeLatest, takeLeading, select } from "@redux-saga/core/effects";
-import { INCREASE_COUNT, DECREASE_COUNT, GET_LATEST_NEWS } from "../constants";
-import { getLatestNews } from "../../api";
-import { setLatestNews } from "../actions/actionCreator";
+import { takeEvery, put, call, fork} from "@redux-saga/core/effects";
+import { GET_NEWS } from "../constants";
+import { getLatestNews, getPopularNews } from "../../api";
+import { setLatestNews, setPopularNews } from "../actions/actionCreator";
 
 export function* handleLatestNews() {
   const { hits } = yield call(getLatestNews, 'react');
   yield put(setLatestNews(hits));
 }
 
+export function* handlePopularNews() {
+  const { hits } = yield call(getPopularNews);
+  yield put(setPopularNews(hits));
+}
+
+export function* handleNews() {
+  // take, call - blockers
+  // fork - not
+  // also we can use `all` and `race` effects (relevant to Promise methods)
+  yield fork(handleLatestNews);
+  yield fork(handlePopularNews);
+}
+
 export function* watchClickSaga() {
-  // yield take(INCREASE_COUNT);
-  yield takeEvery(GET_LATEST_NEWS, handleLatestNews);
-  // yield takeLatest(INCREASE_COUNT, workerSaga);
-  // yield takeLeading(INCREASE_COUNT, workerSaga);
+  yield takeEvery(GET_NEWS, handleNews);
   
 }
 
