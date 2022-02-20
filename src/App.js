7a6ab7b6930ee3080d23f1359dde7927a1c4d679
alex <alexeleave@gmail.com>
2022-02-20 16:08:45 +0400

    Redux Integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increaseCount, decreaseCount } from "./redux/actions/actionCreator";
 
 const App = () => {
-  const store = useSelector(store => store);
-  console.log(store);
+  const count = useSelector(store => store?.counter?.count || 0);
+  const dispatch = useDispatch();
+
+  const handleIncrease = () => {
+    dispatch(increaseCount());
+  }
+
+  const handleDecrease = () => {
+    dispatch(decreaseCount());
+  }
+
   return (
     <div className="App">
-      Hello
+      <button onClick={handleIncrease}>+1</button>
+      <button onClick={handleDecrease}>-1</button>
+      <h1>{count}</h1>
     </div>
   );
 }
